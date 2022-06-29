bbabad0aee8b1815b3a08e8d19946600453a8d2e
BogomolDmitriy <bogomoldima1992@gmail.com>
2022-06-29 13:58:28 +0300

    Add project files.

---
+++ IteaPro_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IteaPro_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
