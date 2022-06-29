65448f085cb9cf66a70dbd6346cc24fb5527f3f4
BogomolDmitriy <93612619+BogomolDmitriy@users.noreply.github.com>
2022-06-29 15:41:08 +0300

    implemented max max arrey (#1)
    * implemented max max arrey
    
    * Update

--- IteaPro_1/Program.cs
+++ IteaPro_1/Program.cs
@@ -4,6 +4,19 @@ namespace IteaPro_1
 {
     class Program
     {
+        static int max (int [] arrey)
+        {
+            int max = arrey[0];
+            for (int i = 0; i < arrey.Length; i++)
+            {
+                if (arrey[i]> max)
+                {
+                    max = arrey[i];
+                }
+            }
+
+            return max;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
